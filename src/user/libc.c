8058a18db1a519d4dd49e8c91bbde2cee9d864d1
Sarah Kaylor <sarah.kaylor@outlook.com>
2026-04-18 15:34:10 -0500

    refactoring of syscall code

--- src/user/libc.c
+++ src/user/libc.c
@@ -1,34 +1,26 @@
 #include "libc.h"
 
+#define SYS_WRITE (1)
+#define SYS_EXIT (2)
+
 static inline long syscall1(long sys_num, long arg0) {
-    register long x8 __asm__("x8") = sys_num;
-    register long x0 __asm__("x0") = arg0;
-    __asm__ volatile (
-        "svc #0\n"
-        : "+r"(x0)
-        : "r"(x8)
-        : "memory"
-    );
-    return x0;
+  register long x8 __asm__("x8") = sys_num;
+  register long x0 __asm__("x0") = arg0;
+  __asm__ volatile("svc #0\n" : "+r"(x0) : "r"(x8) : "memory");
+  return x0;
 }
 
 static inline long syscall0(long sys_num) {
-    register long x8 __asm__("x8") = sys_num;
-    register long x0 __asm__("x0");
-    __asm__ volatile (
-        "svc #0\n"
-        : "=r"(x0)
-        : "r"(x8)
-        : "memory"
-    );
-    return x0;
+  register long x8 __asm__("x8") = sys_num;
+  register long x0 __asm__("x0");
+  __asm__ volatile("svc #0\n" : "=r"(x0) : "r"(x8) : "memory");
+  return x0;
 }
 
-void print(const char* str) {
-    syscall1(1, (long)str);
-}
+void print(const char *str) { syscall1(SYS_WRITE, (long)str); }
 
 void exit(void) {
-    syscall0(2);
-    while(1); // Wait for the kernel to halt us safely
+  syscall0(SYS_EXIT);
+  while (1)
+    ; // Wait for the kernel to halt us safely
 }
