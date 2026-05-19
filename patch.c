547ea9424dce5c9e3ed21da33d2c226517c74287
Sarah Kaylor <sarah.kaylor@outlook.com>
2026-05-19 08:11:05 -0500

    remove debugging files

--- patch.c
+++
@@ -1,9 +0,0 @@
-#include "graphics/window.h"
-extern struct window windows[];
-void print_console(const char *s);
-
-void debug_print_text() {
-    print_console("[DEBUG] Window 0 text: ");
-    print_console(windows[0].text);
-    print_console("\n");
-}
--- trace.py
+++
@@ -1,3 +0,0 @@
-import re
-with open("hobbyos_objdump.txt", "w") as f:
-    f.write("OK")
